77a69760798658e0a5a188989f9df962c59263fd
seunghyun <seunghyun@kroffle.com>
2023-09-22 11:28:33 +0900

    update

--- main.dart.js
+++ main.dart.js
@@ -116983,7 +116983,7 @@ k=A.dW(!1,m,m,m,!0,m,!1,m,m,m,m,!1,m,m,m,m,m,B.a6U,a.ayG(),m)
 p=a.ax
 q=new A.ch().iV().a
 q=A.b3(A.b([k,A.dW(!1,m,m,m,!0,m,!1,m,m,m,new A.agN(a),p==="account",m,A.v(51,q>>>16&255,q>>>8&255,q&255),m,m,m,B.a75,m,m)],n),B.m,B.k,B.n)
-k=A.cD(2023,9,21,19,3,0,0,!1)
+k=A.cD(2023,9,22,11,28,0,0,!1)
 if(!A.c3(k))A.U(A.c7(k))
 $.ho()
 return A.ic(l,A.iX(new A.aR(B.kV,A.b3(A.b([new A.by("\uc571 \uc124\uc815",m,o,m,m),B.pC,B.dT,new A.by("CMS \uc124\uc815",m,q,m,m),B.pC,B.dT,new A.by("\ucd5c\uadfc \uc5c5\ub370\uc774\ud2b8",m,A.at(A.aW3("yyyy. MM. dd HH:mm").vI(new A.be(k,!1)),m,m,m,m,m,m,m),m,m),B.a1x],n),B.m,B.k,B.n),m),m,B.a6),m,m,m)},
@@ -118647,7 +118647,7 @@ A.Y6.prototype={
 E(a){return A.dR(new A.axc(),null,A.aXx(),null,null,t._U)}}
 A.axc.prototype={
 $1(a){var s=null
-return A.ic(A.xV(s,s,B.a78),A.b3(A.b([new A.aR(B.aG,A.eJ(a.db,s,!1,"\uc2a4\ud1a0\uc5b4\uba85 \ub610\ub294 \ud0a4\uc6cc\ub4dc\ub97c \uc785\ub825\ud574\uc11c \uac80\uc0c9","\ud504\ub85c\ubaa8\uc158 \uac80\uc0c9",new A.ax8(a),s,new A.ax9(a)),s),B.b7,A.cM(A.ig(a,new A.axa(a),B.pY,B.bc),1)],t.p),B.m,B.k,B.n),s,s,A.Uu(B.dE,!1,new A.axb()))},
+return A.ic(A.xV(s,s,B.a78),A.b3(A.b([new A.aR(B.aG,A.eJ(a.db,s,!1,"\ud0c0\uc774\ud2c0, \ud0a4\uc6cc\ub4dc, \uc2a4\ud1a0\uc5b4, \ud0c0\uc785  \uc744 \uc785\ub825\ud574\uc11c \uac80\uc0c9","\ud504\ub85c\ubaa8\uc158 \uac80\uc0c9",new A.ax8(a),s,new A.ax9(a)),s),B.b7,A.cM(A.ig(a,new A.axa(a),B.pY,B.bc),1)],t.p),B.m,B.k,B.n),s,s,A.Uu(B.dE,!1,new A.axb()))},
 $S:751}
 A.ax8.prototype={
 $1(a){var s=this.a,r=s.db.a.a
